8f9024a81414cbc7e18ba5c9ad17f472bd9e386c
Administrator <limingbright@yahoo.com>
2016-01-18 23:02:22 +0800

    第一个helloworld

---
+++ src/com/liml/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.liml.HelloWorld;
+
+/**
+ * Created by Administrator on 2016/1/18.
+ */
+public class HelloWorld {
+        public static void main(String[] args){
+            System.out.println("test");
+        }
+}
